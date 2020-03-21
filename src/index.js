4ed68095616487f35737070c1252433560f6e0c2
藤原出帆 <contact@fujishan.jp>
2020-03-21 11:19:35 +0900

    📝 Add help command

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 "use strict";
+
+const program = require("commander");
+
+program
+  .usage("-t TITLE -d DESCRIPTION -o /path")
+  .option("-t, --title <value>", "BOSYU Title", String, "TEST")
+  .option(
+    "-d, --description <value>",
+    "BOSYU Description",
+    String,
+    "This is description."
+  )
+  .option("-o, --out <path> ", "Image output path", String, "/")
+  .parse(process.argv);
