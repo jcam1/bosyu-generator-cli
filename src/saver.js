b4f0e8077085e1363b503144191bfecc49fc0140
Izuho Fujiwara <contact@fujishan.jp>
2020-03-30 18:03:21 +0900

    ♻️  Refactoring saver function

--- src/saver.js
+++ src/saver.js
@@ -1,20 +1,8 @@
-module.exports = (function() {
+module.exports.save = (function(canvas, filepath, callback) {
 	"use strict";
 
 	const fs = require("fs");
+	const buffer = new Buffer(canvas.toDataURL().splot(",")[1], "base64");
 
-	function canvasToBase64(canvas) {
-		return canvas.toDataURL().split(",")[1];
-	}
-
-	function decodeWrite(base64, filepath, callback) {
-		const buffer = new Buffer(base64, "base64");
-		fs.writeFile(filepath, buffer, callback);
-	}
-
-	return {
-		save: function(canvas, filepath, callback) {
-			decodeWrite(canvasToBase64(canvas), filepath, callback);
-		}
-	};
+	fs.writeFile(filepath, buffer, callback);
 })();
